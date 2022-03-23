39b559c960eb0456d0f7562efd368064fd5e9633
Laura-Klery <lklery@yahoo.fr>
2022-03-23 20:11:43 +0100

    Mise à jour

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Experience;
+use App\Entity\Project;
+use App\Entity\Tag;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -26,7 +30,10 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Toutes mes expériences', 'fas fa-briefcase', Experience::class);
+        yield MenuItem::linkToCrud('Tous mes projets', 'fas fa-laptop-code', Project::class);
+        yield MenuItem::linkToCrud('Tous les langages', 'fas fa-code', Tag::class);
     }
 }
